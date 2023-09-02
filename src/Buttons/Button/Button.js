11a47969c13a5114e868c03b94f6873475541646
David Sahakyan <davitsahakyan373@gmail.com>
2023-09-03 02:03:31 +0400

    homework classical components

--- src/Buttons/Button/Button.js
+++ src/Buttons/Button/Button.js
@@ -1,12 +1,20 @@
+import { Component } from 'react'
+
 import './button.css'
 
-const Button = ({ item, x, y,styles = {}}) => {
-    const style = styles
-    style.width = 90*x + "px"
-    style.height = 90*y + "px"
+class Button extends Component{
+    render(){
+        const { item, x, y, styles = {} } = this.props
 
-    return <button className='button' style={style}>
-        <h2>{item}</h2> </button >
+        const style = styles
+        style.width = 90 * x + "px"
+        style.height = 90 * y + "px"
+
+        return <button className='button' style={style}>
+            <h2>{item}</h2> </button >
+    }
+
+
 }
 
 export default Button
--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,27 +1,32 @@
+import { Component } from 'react'
+
 import Button from './Button'
 import './buttons.css'
 
-const Buttons = () => {
-    return <div className='buttons'>
-        <Button item="c" x="1" y="1" />
-        <Button item="<" x="1" y="1" />
-        <Button item="/" x="1" y="1" />
-        <Button item="x" x="1" y="1" />
-        <Button item="7" x="1" y="1" />
-        <Button item="8" x="1" y="1" />
-        <Button item="9" x="1" y="1" />
-        <Button item="-" x="1" y="1" />
-        <Button item="4" x="1" y="1" /> 
-        <Button item="5" x="1" y="1" />
-        <Button item="6" x="1" y="1" />
-        <Button item="+" x="1" y="1" />
-        <Button item="1" x="1" y="1" />
-        <Button item="2" x="1" y="1" />
-        <Button item="3" x="1" y="1" />
-        <Button item="=" x="1" y="2" />
-        <Button item="0" x="2" y="1"/>
-        <Button item="." x="1" y="1" />
-    </div>
+class Buttons extends Component {
+    render() {
+        return <div className='buttons'>
+            <Button item="c" x="1" y="1" />
+            <Button item="<" x="1" y="1" />
+            <Button item="/" x="1" y="1" />
+            <Button item="x" x="1" y="1" />
+            <Button item="7" x="1" y="1" />
+            <Button item="8" x="1" y="1" />
+            <Button item="9" x="1" y="1" />
+            <Button item="-" x="1" y="1" />
+            <Button item="4" x="1" y="1" />
+            <Button item="5" x="1" y="1" />
+            <Button item="6" x="1" y="1" />
+            <Button item="+" x="1" y="1" />
+            <Button item="1" x="1" y="1" />
+            <Button item="2" x="1" y="1" />
+            <Button item="3" x="1" y="1" />
+            <Button item="=" x="1" y="2" />
+            <Button item="0" x="2" y="1" />
+            <Button item="." x="1" y="1" />
+        </div>
+    }
+
 }
 
 export default Buttons
